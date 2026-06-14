1c5afbc1ab4db89c6336137fd8a50a5e0d0e72d2
Henrique Campos <henriquecamposc2006@gmail.com>
2026-06-14 19:41:51 -0300

    feat: add the catalog

--- ecommerce.py
+++ ecommerce.py
@@ -1,1 +1,12 @@
-#Projeto codigo e commerce
+catalogo = {
+    "Produto1": {"nome": "Jaqueta", "preco": 279.79, "categoria": "frio", "quantidade": 10},
+    "Produto2": {"nome": "Calça", "preco": 199.99, "categoria": "frio", "quantidade": 15},
+    "Produto3": {"nome": "Bermuda", "preco": 129.29, "categoria": "calor", "quantidade": 15},
+    "Produto4": {"nome": "Camiseta", "preco": 89.90, "categoria": "calor", "quantidade": 20},
+    "Produto5": {"nome": "Corrente", "preco": 89.90, "categoria": "acessorios", "quantidade": 25},
+    "Produto6": {"nome": "Pulseira", "preco": 79.99, "categoria": "acessorios", "quantidade": 15}
+}
+
+def busca():
+    prod_busca = input("Procure o produto por nome ou categoria: ")
+    for
